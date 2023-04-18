c8b997e212bee605a5b0c4842e2c6ea16c600ec6
Siobhan Donnelly <siobhan.donnelly@hesa.ac.uk>
2023-04-18 14:18:21 +0100

    Cleaning the variable names, then parsing the f_acyear from character to Factor.

--- Accelerator start.R
+++ Accelerator start.R
@@ -10,3 +10,9 @@ library(tidyverse)
 nature_of_work <- read_csv("UKHSA dataset.csv")
 #Taking a look at the dataset tibble
 nature_of_work
+#Cleaning up the variable names
+nature_of_work <- janitor::clean_names(nature_of_work)
+#Starting to amend variable types. Changing the f_acyear variable to a factor
+
+typeof("f_acyear")
+parse_factor(c("2016/17", "2017/18", "2018/19"))

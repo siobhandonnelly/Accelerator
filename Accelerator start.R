d000ad7d900627584f531de02c0f20b95ecf5fe4
Siobhan Donnelly <siobhan.donnelly@hesa.ac.uk>
2023-04-26 10:56:36 +0100

    editing and adding plots

--- Accelerator start.R
+++ Accelerator start.R
@@ -8,10 +8,9 @@ install.packages("janitor", dependencies = TRUE, type = "win.binary")
 library(tidyverse)
 #Loading in the dataset
 #trying to load in data with nulls de\lt with but code not working
-
-nature_of_work <- read_csv("UKHSA dataset.csv", 
-                           na = c("U", "Unknown (IMD)", "Not applicable (IMD)", "Not applicable (NIMMD)", "Unknown (NIMDM)" ,"Not applicable (SIMD)" ,"Unknown (SIMD)", "Not applicable (WIMD)", "Unknown (WIMD)",
-"Z0 - Missing data" ,"NA" , "Not known, "'Not known/Not applicable'", "'Not applicable'", "'03'", "'-1'","'*'", ".", "", "'NULL'"))
+nature_of_work <- read_csv("UKHSA dataset.csv")
+#nature_of_work <- read_csv("UKHSA dataset.csv"), 
+#                           na = c("U", "Unknown (IMD)", "Not applicable (IMD)", "Not applicable (NIMMD)", "Unknown (NIMDM)" ,"Not applicable (SIMD)" ,"Unknown (SIMD)", "Not applicable (WIMD)", "Unknown (WIMD)","Z0 - Missing data" ,"NA" , "Not known, "'Not known/Not applicable'", "'Not applicable'", "'03'", "'-1'","'*'", ".", "", "'NULL'"))
 
 #Taking a look at the dataset tibble
 nature_of_work
@@ -26,5 +25,20 @@ library(usethis)
 ?use_github
 
 use_github(protocol = 'https', auth_token = Sys.getenv("GITHUB_PAT"))
+View(nature_of_work)
 
 
+library(ggplot2)
+
+n_distinct(f_acyear)
+
+ggplot(data = nature_of_work) +
+       geom_point(mapping = aes(x = f_acyear,
+                                y = f_zcohort)
+                  )
+
+
+ggplot(data = nature_of_work) +
+       geom_point(mapping = aes(x = f_acyear,
+                                y = f_zreversedec)
+                  )

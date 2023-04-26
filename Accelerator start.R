f3e668511f78b13bd607a3e4a603785e4e4e02ef
Siobhan Donnelly <siobhan.donnelly@hesa.ac.uk>
2023-04-26 14:09:03 +0100

    Tryin to convert Work Mean variable from a string to a numeric variable

--- Accelerator start.R
+++ Accelerator start.R
@@ -1,19 +1,21 @@
 #Installing the packages used to load in and clean the dataset
 # install.packages("tidyverse", dependencies = TRUE, type = "win.binary")
-# 
 # install.packages("readxl", dependencies = TRUE, type = "win.binary")
-# 
 # install.packages("janitor", dependencies = TRUE, type = "win.binary")
+
 #Loading in the Tidyverse
 library(tidyverse)
-
 library(usethis)
+library(ggplot2)
+library(dplyr)
 ?use_github
-# use_github(protocol = 'https', auth_token = Sys.getenv("GITHUB_PAT"))
-#Loading in the dataset
 
+# use_github(protocol = 'https', auth_token = Sys.getenv("GITHUB_PAT"))
+
+#Loading in the dataset
 nature_of_work <- read_csv("UKHSA dataset.csv")
-#trying to load in data with nulls de\lt with but code not working
+
+#Trying to load in data with nulls de\lt with but code not working
 #nature_of_work <- read_csv("UKHSA dataset.csv"), 
 #                           na = c("U", "Unknown (IMD)", "Not applicable (IMD)", "Not applicable (NIMMD)", "Unknown (NIMDM)" ,"Not applicable (SIMD)" ,"Unknown (SIMD)", "Not applicable (WIMD)", "Unknown (WIMD)","Z0 - Missing data" ,"NA" , "Not known, "'Not known/Not applicable'", "'Not applicable'", "'03'", "'-1'","'*'", ".", "", "'NULL'"))
 
@@ -23,23 +25,29 @@ View(nature_of_work)
 nature_of_work <- janitor::clean_names(nature_of_work)
 #Starting to amend variable types. Changing the f_acyear variable to a factor
 
-typeof("f_acyear")
-
-parse_factor(c("2016/17", "2017/18", "2018/19"))
+# typeof("f_acyear")
+# 
+# parse_factor(c("2016/17", "2017/18", "2018/19"))
 
 View(nature_of_work)
 
-library(ggplot2)
 
-n_distinct(f_acyear)
+nature_of_work %>%
+  mutate(work_mean_num = case_when(f_wrkmean %in% ("Strongly agree") ~ 5,
+                                   f_wrkmean %in% ("Agree")  ~ 4 ,
+                                   f_wrkmean %in% ("Neither agree nor disagree") ~ 3 ,
+                                   f_wrkmean %in% ("Disagree") ~ 2 ,
+                                   f_wrkmean %in% ("Strongly disagree") ~ 1 
+                                   ))
+typeof("work_mean_num")
 
-ggplot(data = nature_of_work) +
-       geom_point(mapping = aes(x = f_acyear,
-                                y = f_zcohort)
-                  )
 
-
-ggplot(data = nature_of_work) +
-       geom_point(mapping = aes(x = f_acyear,
-                                y = f_zreversedec)
-                  )
+# ggplot(data = nature_of_work) +
+#        geom_point(mapping = aes(x = f_acyear,
+#                                y = f_zcohort)
+#                   )
+# 
+# ggplot(data = nature_of_work) +
+#        geom_point(mapping = aes(x = f_acyear,
+#                                 y = f_zreversedec)
+#                   )    

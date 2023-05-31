d9abadd56211bc5c73250f1ea96dc6b63b67d930
Siobhan Donnelly <siobhan.donnelly@hesa.ac.uk>
2023-05-31 14:59:10 +0100

    updating plots to remover year

--- Accelerator start.R
+++ Accelerator start.R
@@ -86,21 +86,20 @@ plot_creation <- function(df, colName) {
                     y = mean_danow,
                     fill = ndf[[colName]]),
       position = position_dodge(),
-    ) +
-       # coord_cartesian(ylim=c(1,5)) +
-        scale_fill_manual(values = c("#1F4388", 
-                                 "#83C7BC", 
+    )  +
+      coord_cartesian(ylim=c(1,5)) +
+        scale_fill_manual(values = c("#1F4388",
+                                 "#83C7BC",
                                  "#1E355E",
                                  "#6A86B8",
                                  "#A93439",
                                  "#CE3162",
                                  "#E57D3A",
                                  "#4EA585",
-                                 "#BBB332",
+                                "#BBB332",
                                  "#E8D77E")
-    ) # adding HESA colours
+   ) # adding HESA colours
 }
-
 
 # Chart 1: Showing the design and nature of work score by the graduates Standard occupational code
 graph1 <- plot_creation(now_update2,"f_xwrk2020soc1") + 
@@ -166,9 +165,47 @@ graph5 <- plot_creation(now_update2,"f_xdomgr01") +
        caption = "")
 graph5
 
+graph6 <- plot_creation(now_update2,"f_xwrk2007sic1") +
+  
+  labs(x = "Graduation Year", # Labels
+       y = "Mean design and nature of work score",
+       fill  = "Standard Industrial Classification (SIC)", # Legend Label
+       title = "Graduates Design and nature of work score by their SIC code",
+       subtitle = "",
+       caption = "")
+graph6
 
 #creating an interactive plot for graph1
 ggplotly(graph1)
+
+plot_creation_without_acyear <- function(df, colName) {
+  ndf <- df %>%
+    filter(!is.na(df[[colName]]))
+  
+  ndf %>% 
+    group_by(ndf[[colName]]) %>%
+    mutate(mean_danow = mean(danow, na.rm = TRUE)) %>% 
+    ggplot() +
+    geom_col(
+      mapping = aes(x = 1,  # Specify a constant value for x
+                    y = mean_danow,
+                    fill = ndf[[colName]]),
+      position = position_dodge(),
+    ) +
+    coord_cartesian(ylim = c(1, 5)) +
+    scale_fill_manual(values = c("#1F4388",
+                                 "#83C7BC",
+                                 "#1E355E",
+                                 "#6A86B8",
+                                 "#A93439",
+                                 "#CE3162",
+                                 "#E57D3A",
+                                 "#4EA585",
+                                 "#BBB332",
+                                 "#E8D77E")
+    )  # adding HESA colours
+}
+Option_1 <- plot_creation_without_acyear(now_update2,"f_xwrk2020soc1")
 
 
 #working to try and replicate the above function but with creating a plot_ly chart. The code is not working yet. This is to see if there is a difference in speed between the two options
@@ -192,46 +229,80 @@ plotly_creation <- function(df, colName) {
 }
 Graph1 <- plotly_creation(now_update2,"f_xwrk2020soc1") 
 
-
+ color_palette <- c("#1F4388",
+                    "#83C7BC",
+                    "#1E355E",
+                    "#6A86B8",
+                    "#A93439",
+                    "#CE3162",
+                    "#E57D3A",
+                    "#4EA585",
+                    "#BBB332",
+                    "#E8D77E")
 #creating the chart by itself is working but not in the function, but we are missing a group in the chart?
-ndf_soc <- now_update2 %>% 
-  group_by(f_zcohort, f_xwrk2020soc1) %>%
-  summarise(mean_danow = median(danow, na.rm = TRUE))
+
+ ndf_soc <- now_update2 %>% 
+   group_by( f_xwrk2020soc1) %>%
+      summarise(mean_danow = median(danow, na.rm = TRUE))
 
  Chart1 <- plot_ly(
-  data= {ndf_soc},
-  x = ~f_zcohort,
-  y = ~mean_danow,
-  color = ~f_xwrk2020soc1,
-  type = "bar",
-  showlegend = FALSE) %>%
-  layout(
-    yaxis = list(title = 'Mean Fairwork score'),
-    xaxis = list(title = 'Acedemic Year',tickvals = list(2017/18, 2018/19)),
-    title = "Mean Fairwork score by graduates SOC major group") %>%
-  hide_colorbar() %>%
-  suppressWarnings()
-
+   data = ndf_soc,
+   x = ~f_xwrk2020soc1,
+   y = ~mean_danow,
+   color = ~f_xwrk2020soc1,
+   type = "bar",
+   showlegend = FALSE,
+   marker = list(color = color_palette)
+ ) %>%
+   layout(
+     yaxis = list(title = 'Mean Fairwork score'),
+     xaxis = list(title = 'SOC group', tickvals = list("Managers, directors and senior officials", "Professional occupations", "Associate professional occupations", "Administrative and secretarial occupations", "Skilled trades occupations", "Caring, leisure and other service occupations", "Sales and customer service occupations", "Process, plant and machine operatives", "Elementary occupations")),
+     title = "Mean Fairwork score by graduates SOC major group"
+   ) %>%
+   hide_colorbar() %>%
+   suppressWarnings()
+ 
  
  #creatin the chart for SIC
  ndf_sic <- now_update2 %>% 
-   group_by(f_zcohort, f_xwrk2007sic1) %>%
+   group_by(f_xwrk2007sic1) %>%
    summarise(mean_danow = median(danow, na.rm = TRUE))
  
  Chart2 <- plot_ly(
    data= {ndf_sic},
-   x = ~f_zcohort,
+   x = ~f_xwrk2007sic1,
    y = ~mean_danow,
    color = ~f_xwrk2007sic1,
    type = "bar",
-   showlegend = FALSE) %>%
+   showlegend = FALSE,
+   marker = list(color = color_palette)) %>%
    layout(
      yaxis = list(title = 'Mean Fairwork score'),
-     xaxis = list(title = 'Acedemic Year',tickvals = list(2017/18, 2018/19)),
+     xaxis = list(title = 'SIC Group',tickvals = list("Agriculture, forestry and fishing", "Mining and quarrying", "Manufacturing", "Electricity, gas, steam and air conditioning supply" , "Water supply; sewerage, waste management and remediation activities", "Construction" , "Wholesale and retail trade; repair of motor vehicles and motorcycles" , "Transportation and storage", "Accommodation and food service activities" , "Information and communication" , "Financial and insurance activities", "Real estate activities", "Professional, scientific and technical activities", "Administrative and support service activities", "Public administration and defence; compulsory social security", "Education", "Human health and social work activities", "Arts, entertainment and recreation", "Other service activities", "Activities of households as employers; undifferentiated goods- and services-producing activities of households for own use", "Activities of extraterritorial organisations and bodies")),
      title = "Mean Fairwork score by graduates SIC major group") %>%
    hide_colorbar() %>%
    suppressWarnings()
  
+ 
+ #creatin the chart for empbasis
+ ndf_f_xempbasis <- now_update2 %>% 
+   group_by( f_xempbasis) %>%
+   summarise(mean_danow = median(danow, na.rm = TRUE))
+ 
+ Chart3 <- plot_ly(
+   data= {ndf_f_xempbasis},
+   x = ~f_xempbasis,
+   y = ~mean_danow,
+   color = ~f_xempbasis,
+   type = "bar",
+   showlegend = FALSE,
+   marker = list(color = color_palette)) %>%
+   layout(
+     yaxis = list(title = 'Mean Fairwork score'),
+     xaxis = list(title = 'Employment Basis',tickvals = list("On a permanent/open ended contract", "On a fixed-term contract lasting 12 months or longer","On a fixed-term contract lasting less than 12 months","Temping (including supply teaching)","On a zero hours contract","Volunteering","On an internship","Other","Not known")),
+     title = "Mean Fairwork score by graduates employment basis") %>%
+   hide_colorbar() %>%
+   suppressWarnings()
  
  
  
